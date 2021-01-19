f53df06cba9534a83e3e55d7bec7fa8622bfc9a6
马浩 <303527871@qq.com>
2021-01-20 00:21:53 +0800

    修改首页显示以及下拉刷新

--- src/pages/home/homelineItem.tsx
+++ src/pages/home/homelineItem.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import HTML from "react-native-render-html";
 
 import Avatar from "../../components/Avatar";
 import { Timelines } from "../../config/interface";
 import { dateToFromNow } from "../../utils/date";
-
+import Screen from "../../config/screen";
 
 
 interface HomeLineItemProps {
@@ -30,12 +30,26 @@ const HomeLineItem: React.FC<HomeLineItemProps> = (props) => {
           <Avatar url={item.account.avatar} />
         </View>
         <View style={styles.name}>
-          <Text>{item.account.display_name}</Text>
-          <Text>{dateToFromNow(item.created_at)}</Text>
+          <Text style={{ fontSize: 16 }}>{item.account.display_name}</Text>
+          <Text style={{ marginTop: 8, color: "#999" }}>{dateToFromNow(item.created_at)}</Text>
         </View>
       </View>
       <View style={styles.content}>
-        <HTML source={{ html: item.content }} tagsStyles={tagsStyles} containerStyle={{ paddingVertical: 10 }} />
+        <HTML source={{ html: item.content }} tagsStyles={tagsStyles} containerStyle={{ paddingVertical: 15 }} />
+      </View>
+      <View style={styles.tool}>
+        <View style={styles.tool_item}>
+          <Image source={require("../../images/turn.png")} style={{ width: 24, height: 22 }} />
+          <Text style={{ fontSize: 16, color: '#9f9f9f', marginLeft: 2 }}>转嘟</Text>
+        </View>
+        <View style={styles.tool_item}>
+          <Image source={require("../../images/comment.png")} style={{ width: 20, height: 20 }} />
+          <Text style={{ fontSize: 16, color: '#9f9f9f', marginLeft: 2 }}>评论</Text>
+        </View>
+        <View style={styles.tool_item}>
+          <Image source={require("../../images/like.png")} style={{ width: 22, height: 22 }} />
+          <Text style={{ fontSize: 16, color: '#9f9f9f', marginLeft: 2 }}>赞</Text>
+        </View>
       </View>
     </View>
   )
@@ -49,16 +63,27 @@ const styles = StyleSheet.create({
   },
   title: {
     flexDirection: 'row',
+    paddingTop: 15,
   },
   avatar: {
     paddingRight: 10,
-    paddingTop: 15,
   },
   name: {
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   content: {
 
+  },
+  tool: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  tool_item: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 })
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { observer } from "mobx-react";
 import { useStores } from "../../store";
@@ -9,7 +9,7 @@ import { navigate } from "../../utils/rootNavigation";
 import { homeLine } from "../../server/timeline";
 import { useRequest } from "../../utils/hooks";
 
-import RefreshList from "../../components/RefreshList";
+import RefreshList, { RefreshState } from "../../components/RefreshList";
 import HomeLineItem from "./homelineItem";
 
 const fetchHomeLine = () => {
@@ -24,8 +24,14 @@ const Home: React.FC<{}> = () => {
 
   const [isLogin, setIsLogin] = useState(false);
   const [listData, setListData] = useState<Timelines[]>([]);
+  const [status, setStatus] = useState<RefreshState>(RefreshState.Idle);
 
   const { data: homeLineData, run: getHomeLineData } = useRequest(fetchHomeLine(), { manual: true });
+
+  const handleRefresh = useCallback(() => {
+    setStatus(status => status = RefreshState.HeaderRefreshing);
+    getHomeLineData();
+  }, [status, getHomeLineData]);
 
   useEffect(() => {
     if(appStore.hostUrl.length > 0 && appStore.token.length > 0) {
@@ -38,10 +44,10 @@ const Home: React.FC<{}> = () => {
 
   useEffect(() => {
     if(homeLineData) {
+      setStatus(RefreshState.Idle);
       setListData(homeLineData);
     }
   }, [ homeLineData ]);
-
   
   if(!isLogin) {
     return (
@@ -56,6 +62,8 @@ const Home: React.FC<{}> = () => {
       <RefreshList 
         data={listData}
         renderItem={({ item }) => <HomeLineItem item={item} />}
+        onRefresh={handleRefresh}
+        refreshState={status}
      />
     </View>
   );
